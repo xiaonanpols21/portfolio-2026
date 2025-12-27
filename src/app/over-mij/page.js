b4208b5167d13fd69a95be170e167aa892865031
xiaonanpols21 <xiaonanpolspols@gmail.com>
2025-12-27 14:25:43 +0100

    add faw busy

--- src/app/over-mij/page.js
+++ src/app/over-mij/page.js
@@ -1,7 +1,20 @@
+import { fetchProjects } from "@/js/fetchProjects";
 import Image from "next/image";
 import SocialMedia from "@/components/socialMedia/link";
 import styles from "./me.module.scss";
-export default function Me() {
+export default async function Me() {
+    const data = await fetchProjects();
+    console.log(data)
+
+    // Chat gpt
+    const tags = [
+        ...new Set(
+            data
+            .flatMap(item => item.acf.tags)
+            .map(tagObj => tagObj.tag)
+        )
+    ];
+
     return (
         <main className={`${styles.main}`}>
             <h1>Xiao Nan Pols</h1>
@@ -21,11 +34,49 @@ export default function Me() {
                 <h2>Front-end developer / Designer</h2>
                 <p>Lorem ipsum dolor sit amet consectetur. Ante pharetra nunc condimentum cras. Ultrices sed nunc ut massa in libero nunc neque. Malesuada placerat tincidunt cras amet. Lorem ipsum dolor sit amet consectetur. Ante pharetra nunc condimentum cras. Ultrices sed nunc ut massa in libero nunc neque. Malesuada placerat tincidunt cras amet.</p>
             </section>
-            <ul>
+            <ul className={`${styles.socialMedia}`}>
                 <li>
-                    <SocialMedia/>
+                    <SocialMedia
+                        name={"Github"}
+                        link={"https://github.com/xiaonanpols21"}
+                        img={"/img/icons/github.svg"}
+                    />
+                </li>
+                <li>
+                    <SocialMedia
+                        name={"Linkedin"}
+                        link={"https://www.linkedin.com/in/xiao-nan-pols-b839951b5/"}
+                        img={"/img/icons/linkedin.svg"}
+                    />
+                </li>
+                <li>
+                    <SocialMedia
+                        name={"Instagram"}
+                        link={"https://www.instagram.com/xiaonanpols21/"}
+                        img={"/img/icons/instagram.svg"}
+                    />
                 </li>
             </ul>
+
+            <section className={`${styles.faq}`}>
+                <h3 className="visually-hidden">Kenmerken</h3>
+                <details>
+                    <summary>toegankelijkheid</summary>
+                </details>
+                <details>
+                    <summary>skills</summary>
+                    <ul className={`${styles.tags}`}>
+                    {tags.map((item, key) => (
+                        <li key={key}>{item}</li>
+                    ))}
+                    </ul>
+                </details>
+                <details>
+                    <summary>creatief</summary>
+                </details>
+            </section>
+       
+
         </main>
 
     )
--- src/components/socialMedia/link/index.js
+++ src/components/socialMedia/link/index.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./socialmedia.module.scss";
-export default function SocialMedia() {
+export default function SocialMedia({name, link, img}) {
     return (
-        <Link href="/" className={`${styles.socialMeidaBtn}`}>
-            Github
+        <Link href={link} className={`${styles.socialMeidaBtn}`} target="_blank">
+            {name}
             <div className={`${styles.socialIcon}`}>
                 <Image
-                    src="/img/icons/github.svg"
+                    src={img}
                     width={24}
                     height={24}
                     alt="Social media icon"
