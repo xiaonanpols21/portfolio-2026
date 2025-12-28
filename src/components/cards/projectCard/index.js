0ce5089bf3cf6ccd807b7a1f696933db301f65a9
xiaonanpols21 <xiaonanpolspols@gmail.com>
2025-12-28 09:17:59 +0100

    busy like project button

--- src/components/cards/projectCard/index.js
+++ src/components/cards/projectCard/index.js
@@ -5,23 +5,26 @@ import styles from "./projectCard.module.scss";
 
 
 export default function ProjectCard({title, goal, img, alt, data, slug}) {
+    
     return (
         <article className={`${styles.article}`} >
-            <Link href={`/projecten/${slug}`}>
+           
                 <h2>{title}</h2>
                 <p>{goal}</p>
                 <Tags data={data} classes={styles.tags}/>
-                <span className={`${styles.moreInfo}`}>Meer info</span>
-                <div>
-                    <Image
-                        src={img}
-                        width={500}
-                        height={500}
-                        alt={alt}
-                    />
-                </div>
-           
-            </Link>
+
+                <Link href={`/projecten/${slug}`}>
+                    <div>
+                        <Image
+                            src={img}
+                            width={500}
+                            height={500}
+                            alt={alt}
+                        />
+                    </div>
+                    <span className={`${styles.moreInfo}`}>Meer info</span>
+                </Link>
+                <button aria-label="Like project knop"></button>
         </article>
     )
 }
